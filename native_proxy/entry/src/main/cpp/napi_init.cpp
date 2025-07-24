9aa168d030523c77484724ea04eb0ff671a37618
Parzival <2833456550@qq.com>
2025-07-24 20:15:43 +0800

    修复了一些问题

--- native_proxy/entry/src/main/cpp/napi_init.cpp
+++ native_proxy/entry/src/main/cpp/napi_init.cpp
@@ -1,37 +1,22 @@
 #include "napi/native_api.h"
+#include <cstdlib>
 
-static napi_value Add(napi_env env, napi_callback_info info)
+
+static napi_value NAPI_Global_proxy(napi_env env, napi_callback_info info)
 {
-    size_t argc = 2;
-    napi_value args[2] = {nullptr};
-
-    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
-
-    napi_valuetype valuetype0;
-    napi_typeof(env, args[0], &valuetype0);
-
-    napi_valuetype valuetype1;
-    napi_typeof(env, args[1], &valuetype1);
-
-    double value0;
-    napi_get_value_double(env, args[0], &value0);
-
-    double value1;
-    napi_get_value_double(env, args[1], &value1);
-
-    napi_value sum;
-    napi_create_double(env, value0 + value1, &sum);
-
-    return sum;
-
+    size_t argc = 1;
+    napi_value argv[1] = {nullptr};
+    napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    
+    return argv[0];
 }
 
 EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports)
-{
+static napi_value Init(napi_env env, napi_value exports) {
     napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
+        {"proxy", nullptr, NAPI_Global_proxy, nullptr, nullptr, nullptr, napi_default, nullptr},
     };
+    
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
--- native_proxy/entry/src/main/cpp/types/libentry/Index.d.ts
+++ native_proxy/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,1 +1,1 @@
-export const add: (a: number, b: number) => number;
+export const proxy: (a: string) => string;
